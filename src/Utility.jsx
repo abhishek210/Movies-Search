35bd699855797b52f7eeb7ed35695806f64a50ba
abhishek yadav <abhishekyadav9667@gmail.com>
2023-03-11 14:41:46 +0530

    project

--- src/Utility.jsx
+++ src/Utility.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const Apim = axios.create({
-    baseURL : "http://www.omdbapi.com/",
+    baseURL : "https://www.omdbapi.com/",
     timeout : 15000,
     params : {
         apikey : "8b66c5da"
